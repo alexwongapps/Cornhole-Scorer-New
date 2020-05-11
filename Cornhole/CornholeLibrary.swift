52effcf1a52b4ba7665e05132bcc9db555cc93a2
Alexander Wong <alexjwong@berkeley.edu>
2020-05-11 12:57:34 -0700

    Added copying of league id

--- Cornhole/CornholeLibrary.swift
+++ Cornhole/CornholeLibrary.swift
@@ -1657,3 +1657,50 @@ func createBasicAlert(title: String, message: String) -> UIAlertController {
     
     return alert
 }
+
+// ui label
+
+class KGCopyableLabel: UILabel {
+
+    override public var canBecomeFirstResponder: Bool {
+        get {
+            return true
+        }
+    }
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setup()
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        setup()
+    }
+
+    func setup() {
+        isUserInteractionEnabled = true
+        addGestureRecognizer(UILongPressGestureRecognizer(
+            target: self,
+            action: #selector(showCopyMenu(sender:))
+        ))
+    }
+
+    override func copy(_ sender: Any?) {
+        UIPasteboard.general.string = text
+        UIMenuController.shared.setMenuVisible(false, animated: true)
+    }
+
+    @objc func showCopyMenu(sender: Any?) {
+        becomeFirstResponder()
+        let menu = UIMenuController.shared
+        if !menu.isMenuVisible {
+            menu.setTargetRect(bounds, in: self)
+            menu.setMenuVisible(true, animated: true)
+        }
+    }
+
+    override func canPerformAction(_ action: Selector, withSender sender: Any?) -> Bool {
+        return (action == #selector(copy(_:)))
+    }
+}
